3d8bc9381dfbde4dd248636f2f00f880de32a8d0
LaKHamote <lucascboa@gmail.com>
2023-10-16 21:53:38 -0300

    ⚡️ Todas as logicas

--- cmdLogic.py
+++ cmdLogic.py
@@ -4,11 +4,12 @@ opcodes = ["add","addi","addiu","addu","and","andi","beq","bgez","bgezal","bne",
 
 
 # 6 particoes:
-       # com 3 registradores (ordem: rd,rs,st)
+       # com ate 3 registradores
               #          6bits    5bits  5bits  5bits  5bits   6bits
 opcodesR = [
-# params ordem:     rd,rs,rt
-# cmds com:                                                   funct
+    
+       {"typeRA": [#registradores:  rd,rs,rt
+       # cmds com:                                            funct
               "add",   # 000000    rs     rt     rd    00000   0x20
               "addu",  # 000000    rs     rt     rd    00000   0x21
               "and",   # 000000    rs     rt     rd    00000   0x24
@@ -20,59 +21,81 @@ opcodesR = [
               "sub",   # 000000    rs     rt     rd    00000   0x22
               "subu",  # 000000    rs     rt     rd    00000   0x23
               "xor",   # 000000    rs     rt     rd    00000   0x26
+       ]},
 
-# params ordem:     rd
-# cmds com:                                                   funct
-              "mfhi",  # 000000   00000  00000   rd    00000   0x10
-              "mflo",  # 000000   00000  00000   rd    00000   0x12
-
-# params ordem:     rd,rs,rt
-# cmds com:            opcode                                 funct
-              "mul",   # 0x1c      rs     rt     rd    00000   0x2
-
-# params ordem:     rs
-# cmds com:            opcode                                 funct
-              "clo",   # 0x1c      rs    00000   rd    00000   0x21
-
-# params ordem:     rs,rt
-# parametros nao comecam com rd:                              funct
+       {"typeRB": [#registradores: rs,rt
+       # cmds com:                                            funct
               "div",   # 000000    rs     rt    00000  00000   0x1a          
               "mult",  # 000000    rs     rt    00000  00000   0x18
+       ]},
 
-# params ordem:     rs
-# parametros nao comecam com rd:                              funct
+       {"typeRC": [#registradores: rs
+       # cmds com:                                            funct
               "jr",    # 000000    rs    00000  00000  00000   0x8
+       ]},
 
+       {"typeRD": [#registradores:  rd,rs,rt
+       # cmds com:             opcode                         funct
+              "mul",   # 0x1c      rs     rt     rd    00000   0x2
+       ]},
 
-# params ordem:     rd,rt,rs/sa ??????????????????
-# cmds com:                                            shamt  fucnt
+       {"typeRE": [#registradores: rd,rs
+       # cmds com:      opcode                                funct
+              "clo",   # 0x1c      rs    00000   rd    00000   0x21
+       ]},
+
+       {"typeRF": [#registradores: rd,rt,rs/sa ??????????????????
+       # cmds com:                                     shamt  fucnt
               "sll",   # 000000    rs     rt     rd     sa      0
               "sra",   # 000000   00000   rt     rd     sa     0x3
               "srav",  # 000000    rs     rt     rd    00000   0x7
               "srl",   # 000000    rs     rt     rd     sa     0x2
+       ]},
+
+       {"typeRG": [#registradores: rd
+       # cmds com:                                                   funct
+              "mfhi",  # 000000   00000  00000   rd    00000   0x10
+              "mflo",  # 000000   00000  00000   rd    00000   0x12
+       ]},
 ]
 
 
 
 # 4 particoes:
        # com 2 registradores
-              #          6bits    5bits  5bits             16bits
+              #           6bits      5bits     5bits          16bits
 opcodesI = [
-              "addi",  #   
-              "addiu", # 
-              "andi",  # 
-              "beq",   # 
-              "bgez",  # 
-              "bgezal",# 
-              "bne",   # 
-              "lb",    # 
-              "lui",   # 
-              "lw",    # 
-              "ori",   # 
-              "sb",    # 
-              "slti",  # 
-              "sw",    # 
-              "xori"   # 
+    
+       {"typeIA": [#parametros: rt,rs,imm
+              "addi",  #   0x8        rs        rt              imm  
+              "addiu", #   0x9        rs        rt              imm   
+              "andi",  #   0xc        rs        rt              imm   
+              "ori",   #   0xd        rs        rt              imm   
+              "slti",  #   0xa        rs        rt              imm   
+              "xori"   #   0xe        rs        rt              imm   
+       ]},
+
+       {"typeIB": [#parametros: rs,rt,offset 
+              "beq",   #   0x4        rs        rt             offset   
+              "bne",   #   0x5        rs        rt             offset   
+       ]},
+
+       {"typeIC": [#parametros: rt, offset(rs)
+              "lb",    #  0x20        rs        rt             offset   
+              "lw",    #  0x23        rs        rt             offset   
+              "sb",    #  0x28        rs        rt             offset   
+              "sw",    #  0x2b        rs        rt             offset   
+       ]},
+
+       {"typeID": [#parametros: rt, imm
+              "lui",   #   0xf       00000      rt              imm   
+       ]},
+
+       {"typeIE": [#parametros: rt, offset
+              "bgez",  #   0x1        rs        0x1             offset   
+              "bgezal",#   0x1        rs       0x21             offset       
+       ]},
+
 ]
 
 # 2 particoes:
@@ -82,10 +105,3 @@ opcodeJ = [
               "j",     #  0x2              target
               "jal"    #  0x3              target  
 ]
-'''
-		addi	$s7,$s7,-1			#
-		jal 	readNotNullByte			# releio o ultimo byte lido, que pode ser ' ' ou ',' ou '\n'
-		addi	$s7,$s7,1			# retorno o ponteiro pro valor atual
-		beq	$v1,10,noReg			# se era '\n' nao ha mais registrador e ja pulamos a linha
-		jal 	readNotNullByte
-       '''
