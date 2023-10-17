dfd567cef8180c7b61820ac47ae420f2e87d3361
LaKHamote <lucascboa@gmail.com>
2023-10-17 20:02:38 -0300

    🐛 getValueAddr: cond. d parada errada $a3 param

--- main.asm
+++ main.asm
@@ -3,14 +3,19 @@
 		#move	$a0,$v1
 		#jal 	printChar
 		#j	end
-add $t0,                                   $s0, $s1           
-addu $t1,      $s2, $s3   
-   xor $t2,    $s4, $s5           
-
-  div         $t3,   $t7
-
-
-
+      addu   $t1,     $s2,     $s3
+      sltu   $t7,     $s6,     $s2
+         and    $t2     $s4,     $s5
+      movn   $t3     $s6,     $s7
+      mult   $t3     $s5   
+         or     $t5,     $s0,     $s5
+      slt    $t6,     $s4     $s1
+      nor    $t4,     $s3,     $s6
+      add    $t0,     $s0    $s1
+      sub    $t8,     $s7     $s3
+         subu   $t9,        $s6,     $s5
+      xor    $t6,     $s5,     $s0
+      div    $t2     $s1     
 
 
 		
@@ -63,19 +68,20 @@ textSection:	# provavelmente a primeira coisa eh cmd -> cmd $xx,$yy,$zz
 typeRA:		la	$a0,valuesRA
 		la	$a1,keysRA
 		li	$a2,1				# numero de values por key
+		li	$a3,' '
 		jal	getValueAddr			# pega o endereco relativo a chave passada
 		beq	$v0,$zero,typeRB
 		lb	$s0,($v0)			# pego o funct do cmd lido
 # pra cima, li o cmd e peguei seu codigo unico -> funct
-
+		li	$a3,','
 		jal 	getRegCode
 		move	$s2,$v0			# salvo o codigo do registrador rd lido
 # pra cima, li o primeito regs e peguei seu codigo unico -> rd
-		
+		li	$a3,','
 		jal 	getRegCode
 		move	$s4,$v0			# salvo o codigo do registrador rs lido
 # pra cima, li o segundo regs e peguei seu codigo unico -> rs
-
+		li	$a3,10
 		jal 	getRegCode
 		move	$s3,$v0			# salvo o codigo do registrador rt lido
 # pra cima, li o terceiro regs e peguei seu codigo unico -> rt
@@ -111,12 +117,15 @@ typeRB:		addi	$s7,$s7,-1		# volto o ponteiro do arquivo para reler
 		la	$a0,valuesRB
 		la	$a1,keysRB
 		li	$a2,1			# numero de values por key
+		li	$a3,' '
 		jal	getValueAddr		# pega o endereco relativo a chave passada
 		beq	$v0,$zero,typeRC
 		lb	$s0,($v0)		# funct
-		
+		li	$a3,','
 		jal 	getRegCode
 		move	$s4,$v0			# rs
+		
+		li	$a3,10
 		jal 	getRegCode
 		move	$s3,$v0			# rt
 		
@@ -234,7 +243,8 @@ endInColon:	# ve se uma palavra termina em ':', retorna 1 caso Vdd e 0 Falso
 noColon:	jr	$ra
 		
 #!!!!!!!!! checar se os $t estao mantendo seus valores originais        
-getValueAddr:	# le a palavra atual e retorna, se achar, o ponteiro para o(s) valor(es) em $v0(+$a2) ou 0 se nao achar
+getValueAddr:	# recebe $a0(array dos valores), $a1(array das chaves), $a2(num de valor por chave) e $a3(indicador do final da palavra)
+				# le a palavra atual e retorna, se achar, o ponteiro para o(s) valor(es) em $v0(+$a2) ou 0 se nao achar
 		move	$v0,$zero	      		# a principio assumo que nao achou e altero $v0 apenas se achar
 		move	$t7,$s7				# salvo o endereco que estou lendo para comparar do inicio para os proximos comandos da string
 		move	$t0,$a0				# ponteiro para indice do array dos opcodes (Hexas)
@@ -243,15 +253,13 @@ checkByte:	lb	$t2,($t1)			# byte lido da string de comandos
 		lbu	$t3,($s7)			# byte lido do arq
 		addi	$s7,$s7,1
 		beq	$t3,' ',match			# SE achamos ' ', entao ja lemos palavra toda e temos um match
-		beq	$t3,',',match			# OU se achamos ',', entao ja lemos palavra toda e temos um match
-		beq	$t3,10,match			# OU se achamos '\n', entao ja lemos palavra toda e temos um match
+		beq	$t3,$a3,match			# OU se achamos nosso indicador - pode ser ',' ou '\n' ou ')'
 		bne	$t2,$t3,nextKey	
 		addi	$t1,$t1,1		
 		j 	checkByte		
-findNextKey:	addi	$t1,$t1,1
+findNextKey:	beq	$t2,0,notFound			# se achar zero, ja leu a string toda sem achar o comando
+		addi	$t1,$t1,1
 		lb	$t2,($t1)
-		#move	$s7,$t7				# !!!! resolve, mas n eh eficiente
-		beq	$t2,0,notFound			# se achar zero, ja leu a string toda sem achar o comando
 nextKey:	bne	$t2,',',findNextKey
 		addi	$t1,$t1,1
 		add	$t0,$t0,$a2			# pulo os valores da chave atual e indexo os da prox chave
@@ -262,19 +270,22 @@ match:		bne	$t2,',',nextKey			# por convencao as chaves terminam em ','. Entao g
 notFound:	jr 	$ra
 
 #!!! ainda nao ta prft, se tiver espaco entre reg e ',', ele nao consegue compilar	
-getRegCode:	# le o registrador atual, se achar, e retorna o codigo em $v0 ou da erro por falta de parametros(registradores)
+getRegCode:	# recebe em $a3 o indicador do getValueAddr apenas para repassar para ele
+			# le o registrador atual, se achar, e retorna o codigo em $v0 ou da erro por falta de parametros(registradores)
+		#move	$t3,$a3
 		addi	$sp,$sp,-4
 		sw	$ra,0($sp)
 		jal 	readNotNullByte	
-		beq	$v1,10,printErrorMsg		# se ler um '\n', ! poucos parametros
+		#beq	$v1,10,printErrorMsg		# se ler um '\n', ! poucos parametros !!!!!!!!!!!!!!!!OBS:ACHO QUE PODE RETIRAR COM A ADICAO DO $A3
 		bne	$v1,'$',printErrorMsg		# registradores comecam com $
 		lbu	$t1,($s7)
 		bltu	$t1,48,printErrorMsg		# aqui lemos um caracter que nao existe nos registradores
 		la	$a1,regKeysNaN			# assumo que o registrador esta escrito com seu 'apelido':  $9 -> $t2
-		bge	$t1,58,keepKeys			# aqui lemos um caracter nao numerico entao acertamos na assun��o acima
+		bgeu	$t1,58,keepNaNKeys		# aqui lemos um caracter nao numerico entao acertamos na assun��o acima
 		la	$a1,regKeysNum			# aqui lemos um caracter nao numerico entao erramos na assun��o
-keepKeys:	la	$a0,regValues			# agr temos que achar o valor desse registrador
+keepNaNKeys:	la	$a0,regValues			# agr temos que achar o valor desse registrador
 		li	$a2,1				# numero de values por key
+		#move	$a3,$t3
 		jal	getValueAddr			# pega o endereco relativo a chave passada
 		beq	$v0, $zero,printErrorMsg	# registrador inexistente
 		lb	$v0,($v0)
@@ -311,7 +322,6 @@ assemblerR:	# desempilho a pilha e monto o codigo de instrucao tipo R em $v0: op
 .data
 	filePath: 	.asciiz 	"D:/example_saida.asm"
 	fileWords: 	.space  	1024
-	errorMsg:	.asciiz 	"Comando nao reconhecido"
 	regKeysNum:	.asciiz		"0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,"
 	regKeysNaN:	.asciiz		"zero,at,v0,v1,a0,a1,a2,a3,t0,t1,t2,t3,t4,t5,t6,t7,s0,s1,s2,s3,s4,s5,s6,s7,t8,t9,k0,k1,gp,sp,fp,ra,"
 	regValues:	.byte		0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
@@ -319,3 +329,4 @@ assemblerR:	# desempilho a pilha e monto o codigo de instrucao tipo R em $v0: op
 	valuesRA:	.byte		0x20,0x21,0x24,0xb,0x27,0x25,0x2a,0x2b,0x22,0x23,0x26	
 	keysRB:		.asciiz		"div,mult,"
 	valuesRB:	.byte		0x1a,0x18
+	errorMsg:	.asciiz 	"Comando nao reconhecido"
