3898139c684ed9755cafe09f6cc80130d74cc9a8
LaKHamote <lucascboa@gmail.com>
2023-10-28 15:53:34 -0300

    ✅

--- main.asm
+++ main.asm
@@ -1,49 +1,54 @@
 .data
-vect1: .word  0x1234
-vect2: .word 0x5678
-vect3: .word 0x9abc
-vect4: .word 0xdef0
-vect5: .word 0x1357
-vect6: .word 0x2468
+vect1:      .word   0x1234
+vect2:      .word   0x5678
+vect3:      .word   0x9abc
+vect4:      .word   0xdef0
+vect5:      .word   0x1357
+vect6:      .word   0x2468
 
 .text
-Label4:add $t0, $s0, $s1
-addi $t1, $s2, 10
-addiu $t2, $s3, 5
-addu $t3, $s4, $s5
-and $t4, $s6, $s7
-Label2:andi $t5, $s4, 255
-beq $s0, $s1, Label5
-bgez $s2, Label2
-bgezal $s3, Label3
-bne $s4, $s5, Label4
-clo $t6, $s6
-div $s7, $s4
-j Label5 
-Label1: nor $t4, $s4, $s0
-jal Label6
-jr $ra
-lui $t8, 0xffff
-mfhi $t0
-Label6: slt $t9, $s5, $s6
-mflo $t1
-Label3:movn $t2, $s2, $s3
-mul $t3, $s4, $s5
-mult $s6, $s7
-sll $t8, $s4, 2
-slti $t0, $s7, 10
-sltu $t1, $s4, $s0
-sra $t2, $s0, 3
-srav $t3, $s1, $s2
-Label5:srl $t4, $s3, 2
-sub $t5, $s4, $s5
-subu $t6, $s6, $s7
- or $t5, $s0, $s1
-xor $t8, $s0, $s0
-xori $t9, $s1, 255
-ori $t6, $s2, 255
-sw $t7, vect1($s4)
-lw $t9, vect5($s1)
+Label4:     add     $31,$5,$s1  
+            addi    $t1,$s2,10 
+            addiu   $t2,$v0,5   
+            addu    $t3,$a3,$s5
+            and     $t4,$s6,$s7
+Label2:     
+            andi    $t5, $s4, 255
+            beq     $s0, $s1, Label5
+            bgez    $s2, Label2    
+            bgezal  $s3, Label3 
+            bne     $s4, $s5, Label4
+            clo     $t6, $s6
+
+            div     $7, $s4
+            j       Label5 
+Label1:     nor     $v1, $s4, $s0
+            jal     Label6
+            jr      $ra
+            lui     $a1, 0xffff
+            mfhi    $t0
+Label6:     slt     $t9, $s5, $7
+            mflo    $t1
+Label3:     movn    $t2, $s2, $s3
+            mul     $sp, $s4, $s5
+            mult    $22, $s7
+            sll     $t8, $s4,   2
+            slti    $t0, $s7, 0
+            sltu    $fp, $s4, $s0
+            sra     $t2, $s0, 3 
+
+            srav    $t3, $s1, $s2
+Label5:     srl     $at, $s3, 2  
+            sub     $zero, $s4, $s5
+            subu    $t6, $s6, $s7 
+            or      $t5, $s0, $s1
+            xor     $t8, $s0, $s0 
+            xori    $t9, $3, 255 
+            ori     $t6, $s2, 255
+
+            sw      $0, vect1($s4)   
+            lw      $t9, vect5($s1) 
+
 
 
 
