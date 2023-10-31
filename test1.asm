9a88930e8923c3383b7ca9ba3bfa0839928fa626
LaKHamote <lucascboa@gmail.com>
2023-10-30 22:05:28 -0300

    🎨

--- test1.asm
+++ test1.asm
@@ -8,7 +8,7 @@ vect6:      .word   0x2468, 0x0
 
 .text
 Label4:     add     $31,$5,$s1
-            addi    $t1,$s2,0x10a 
+            addi    $t1,$s2,  0x10a 
             addiu   $t2,$v0,5   
             addu    $t3,$a3,$s5
             and     $t4,$s6,$s7
@@ -20,10 +20,9 @@ Label2:
             bgezal  $s3, Label3 
             bne     $s4, $s5, Label4
             clo     $t6, $s6
-
-            div     $7, $s4
+            div     $7 $s4
             j       Label5 
-Label1:     nor     $v1, $s4, $s0
+Label1:     nor     $v1, $s4,$s0
             jal     Label6
             jr      $ra
             lui     $a1, 0xffff
@@ -37,7 +36,7 @@ Label3:     movn    $t2, $s2, $s3
             slti    $t0, $s7, 0
             sltu    $fp, $s4, $s0
             sra     $t2, $s0, 3 
-
+            
             srav    $t3, $s1, $s2
 Label5:     srl     $at, $s3, 2  
             sub     $zero, $s4, $s5
@@ -47,8 +46,8 @@ Label5:     srl     $at, $s3, 2
             xori    $t9, $3, 0xff
             ori     $t6, $s2, 255
 
-            sw      $0, vect1($s4)   
-            lw      $t9, vect5($s1) 
+            sw $0 vect1($s4)   
+            lw $t9, vect5($s1) 
 
-            sb      $t7, -99999999($s3)  
-            lb      $k0, 100($s0)
+            sb $t7, -99999999($s3)  
+            lb $k0, 100($s0)
